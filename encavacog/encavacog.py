691c521121712952636da447708d4d3a499adbd6
Juan Hernandez <juanlh182@gmail.com>
2024-11-01 11:49:03 -0400

    test: avoid connecting to voice channel

--- encavacog/encavacog.py
+++ encavacog/encavacog.py
@@ -80,25 +80,25 @@ class EncavaCog(
                         "I don't have permission to connect and speak in your channel."
                     ),
                 )
-            await lavalink.connect(
-                channel=actual_context.author.voice.channel,
-                self_deaf=True,
-            )
-        except AttributeError as e:
-            return await self.send_embed_msg(
-                actual_context,
-                title=_("Unable To Play Tracks"),
-                description=_("Connect to a voice channel first."),
-            )
-        except NodeNotFound as e:
-            return await self.send_embed_msg(
-                actual_context,
-                title=_("Unable To Play Tracks"),
-                description=_(
-                    "Connection to Lavalink node has not yet been established."),
-            )
-        except Exception as e:
-            raise e
+        #     await lavalink.connect(
+        #         channel=actual_context.author.voice.channel,
+        #         self_deaf=True,
+        #     )
+        # except AttributeError as e:
+        #     return await self.send_embed_msg(
+        #         actual_context,
+        #         title=_("Unable To Play Tracks"),
+        #         description=_("Connect to a voice channel first."),
+        #     )
+        # except NodeNotFound as e:
+        #     return await self.send_embed_msg(
+        #         actual_context,
+        #         title=_("Unable To Play Tracks"),
+        #         description=_(
+        #             "Connection to Lavalink node has not yet been established."),
+        #     )
+        # except Exception as e:
+        #     raise e
         player = lavalink.get_player(guild.id)
         player.store("notify_channel", interaction.channel.id)
         await self._eq_check(actual_context, player)
